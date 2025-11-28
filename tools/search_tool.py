b61a27c49e8cb3a48fa1b0889195d0640bd6dc61
John Doe <emrecaglar0505@gmail.com>
2025-11-28 16:00:56 +0300

    arama_sorgu.py dosyası temeli atıldı

---
+++ tools/search_tool.py
@@ -0,0 +1,8 @@
+from langchain.tools import DuckDuckGoSearchRun
+
+search = DuckDuckGoSearchRun()
+
+if __name__ == "__main__":
+    query = "Ankara hava durumu nedir ?"
+    result = search.run(query)
+    print(f"Search results for '{query}':\n{result}")
